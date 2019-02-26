b683cd2bf7d63e27a2a5a952b759d604b66f6b7a
Unknown <kaihulme97@gmail.com>
2019-02-26 15:13:52 +0000

    Dynamic P Allocation for Each Set (with bug)
    Bug where error difference for p+1 is small so it uses p not to overfit, but p+2 would have fit better so must add functionality to check forward 2 degrees of p.

--- lsr.py
+++ lsr.py
@@ -6,6 +6,7 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
+# maps points on least squares regression function
 def map_functions(data, coefficients):
     x_mappings = np.zeros((len(data), 100))
     y_mappings = np.zeros((len(data), 100))
@@ -17,7 +18,7 @@ def map_functions(data, coefficients):
                 y_mappings[i][j] += coefficients[i][k] * (x_mappings[i][j]**k)
     return x_mappings, y_mappings
 
-#plots linear sse regression line
+# plots least squares regression line
 def plot_graph(data, coefficients):
     x_mappings, y_mappings = map_functions(data, coefficients)
     fig, ax = plt.subplots()
@@ -27,7 +28,7 @@ def plot_graph(data, coefficients):
         ax.plot(x_mappings[i], y_mappings[i], c='r')
     plt.show()
 
-# calculates sum sqaured error of a set of data
+# calculates sum squared error of a set of data
 def sse(set, set_coefficients):
     sse = 0
     for i in range(len(set)):
@@ -37,16 +38,17 @@ def sse(set, set_coefficients):
         sse += np.square(yi - set[i][1])
     return sse
 
-# calculates sum of all sse
+# calculates sum of all sum squared errors
 def sum_sse(data, coefficients):
     sum = 0
     for i in range(len(data)):
-        print("sse for data set", i, ":", sse(data[i], coefficients[i]))
-        sum += sse(data[i], coefficients[i])
+        i_sse = sse(data[i], coefficients[i])
+        print("sse for data set", i, ":", i_sse)
+        sum += i_sse
     return sum
 
-# calculates least squares regression line coefficients
-def least_squares(x, y, p):
+# calculates least squares regression line coefficients for given p
+def least_squares(x, y, p, max_p):
     X = np.column_stack((np.ones((x.shape[0], 1)), x))
     if (p>1):
         for i in range(1,p):
@@ -55,7 +57,31 @@ def least_squares(x, y, p):
                 x_exp[j] = x[j]**(i+1)
             X = np.column_stack((X, x_exp))
     A = np.linalg.inv(X.T.dot(X)).dot(X.T).dot(y)
+    for i in range(p, max_p):
+        A = np.append(A, 0)
+    pprint(A)
     return A
+
+# get coefficients for set with optimal p
+def get_coefficients(set, p, max_p, p0_coefficients, p0_sse):
+    p1_coefficients = least_squares(set[:,0], set[:,1], p+1, max_p)
+    p1_sse = sse(set, p1_coefficients)
+    print("P:", p, "p",p,":", p0_sse, "p",p+1,":", p1_sse, "%diff:", p0_sse/p1_sse)
+    if (p<max_p and (p0_sse/p1_sse > 1)):
+        p0_coefficients = get_coefficients(set, p+1, max_p, p1_coefficients, p1_sse)
+    return p0_coefficients
+
+# gets sse and coefficients for data
+def sse_handler(p, max_p, data):
+    coefficients = np.zeros((len(data), max_p+1))
+    for i in range(len(data)):
+        p0_coefficients = least_squares(data[i][:,0], data[i][:,1], p, max_p)                  # calculate coefficients for linear
+        p0_sse = sse(data[i], p0_coefficients)                                          # calculate sse for linear
+        coefficients[i] = get_coefficients(data[i], p, max_p, p0_coefficients, p0_sse)            # return coefficients with least error & non-overfitting
+
+        pprint(coefficients[i])
+
+    return sum_sse(data, coefficients), coefficients
 
 # extracts data from files into array of data sections
 def file_handler(file):
@@ -63,18 +89,12 @@ def file_handler(file):
     return np.array([D[i * 20:(i + 1) * 20] for i in range((len(D) + 20 - 1) // 20 )])
 
 # handles sse calculations and plotting calls
-def sse_handler(plot, file):
-
-    # allow p to be dynamically chosen for best regression per section
-    # do not allow for p to become too large to decrease overfitting
-    p=2
-    # when p>4 plotting errors occur for adv files?
-
+def data_handler(plot, file):
     data = file_handler(file)
-    coefficients = np.zeros((len(data), p+1))
-    for i in range(len(data)):
-        coefficients[i] = least_squares(data[i][:,0], data[i][:,1], p)
-    print("TOTAL SSE:", sum_sse(data, coefficients))
+    start_p = 1 # start with linear regression
+    max_p = 5  # maximum p to stop overflow errors
+    error, coefficients = sse_handler(start_p, max_p, data)
+    print("TOTAL SSE: ",error)
     if (plot):
         plot_graph(data, coefficients)
 
@@ -102,7 +122,7 @@ def args_handler(args):
 def main(args):
     argsValid, plot, file = args_handler(args)
     if (argsValid):
-        sse_handler(plot, file)
+        data_handler(plot, file)
     else:
         print("ERROR: please enter valid arguments")
 
